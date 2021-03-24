48ab3fb42e9a930f5e47465b01d433e789de5905
Niles87 <nbing19@gmail.com>
2021-03-23 18:17:23 -0700

    updated style sheet, added conditional rendering to app js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { sections } from './utils/sections';
+import About from './components/About';
+import ContactForm from './components/Contact';
+import Portfolio from './components/Portfolio';
+import Resume from './components/Resume';
 
 function App() {
+  const [titles] = useState([...sections]);
+  const [currentSection, setCurrentSection] = useState(titles[0]);
   return (
     <div className='App'>
-      <Header />
-      <section></section>
+      <Header
+        sections={titles}
+        currentSection={currentSection}
+        setCurrentSection={setCurrentSection}
+      />
+      <main>
+        {currentSection === 'about me' && <About />}
+        {currentSection === 'contact' && <ContactForm />}
+        {currentSection === 'portfolio' && <Portfolio />}
+        {currentSection === 'resume' && <Resume />}
+      </main>
       <Footer />
     </div>
   );
