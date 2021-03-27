877861dd19b495d046ab955b432107abbedfab39
Niles87 <nbing19@gmail.com>
2021-03-27 14:56:47 -0700

    Updates

---
+++ precache-manifest.7300b272b5cfa9a9d28c8f219e1f5532.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e54a21ff5a36f23d79a549bf848d95de",
+    "url": "/psychic-octo-port/index.html"
+  },
+  {
+    "revision": "602e6dea15f96d01aec5",
+    "url": "/psychic-octo-port/static/css/main.183b0fcf.chunk.css"
+  },
+  {
+    "revision": "a5a2e61329f05b542f14",
+    "url": "/psychic-octo-port/static/js/2.fa2d5e9b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/psychic-octo-port/static/js/2.fa2d5e9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "602e6dea15f96d01aec5",
+    "url": "/psychic-octo-port/static/js/main.67be2974.chunk.js"
+  },
+  {
+    "revision": "7cc747cfa0dfc178b61e",
+    "url": "/psychic-octo-port/static/js/runtime-main.1fb2f697.js"
+  }
+]);
